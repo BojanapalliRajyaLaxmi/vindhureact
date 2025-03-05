c4c853763cc379e99b3a6748afaaa06860deee90
Rajya Laxmi <161436193+BojanapalliRajyaLaxmi@users.noreply.github.com>
2025-03-05 17:18:20 +0530

    Update Cart.jsx

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "cart.css";
+import "./cart.css";
 
 const Cart = () => {
   const navigate = useNavigate();
